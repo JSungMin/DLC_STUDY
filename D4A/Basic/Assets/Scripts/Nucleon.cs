59c8ae5b4e3604f61f54676b0724c6dc448ed178
SungMin <jsm1505104@gmail.com>
2018-10-03 19:00:39 +0900

    D4A #Basic Frames Per Second Added
    - FPS 개념 이해
    - System.Serializable의 용례 확인
    - ToString으로 인한 Overhead 최적화

---
+++ D4A/Basic/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();    
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
