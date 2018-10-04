4ce8246ca99c563ee7236385dbdc8ddebb76ba51
challenger71498 <43464986+challenger71498@users.noreply.github.com>
2018-10-04 18:58:33 +0900

    FPS
    min

---
+++ MIN/Basic/UnityStudy_FramesPerSecond/Assets/FPSCounter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+    
+    public int FPS { get; private set; }
+    public int AverageFPS { get; private set; }
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+
+    public int frameRange = 60;
+
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    private void Update()
+    {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    private void InitializeBuffer()
+    {
+        if(frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0; 
+    }
+
+    private void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange)
+        {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    private void CalculateFPS()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++)
+        {
+            int fps = fpsBuffer[i];
+            sum += fps;
+            if (fps > highest)
+            {
+                highest = fps;
+            }
+            if (fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+}
---
+++ MIN/Basic/UnityStudy_FramesPerSecond/Assets/NucleonSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
